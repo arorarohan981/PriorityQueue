9ab69b7912b5db270fd1910a370095fae0a1809c
Rohan Arora <arorarohan4@gmail.com>
2021-11-07 16:30:16 +0530

    Completed Implementation of Priority Queue.

--- PriorityQueue/PriorityQueue/PriorityQueue.hpp
+++ PriorityQueue/PriorityQueue/PriorityQueue.hpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <memory>
+#include <exception>
 #include <list>
 
 namespace mylib{
@@ -22,6 +23,12 @@ private:
     int dataCount{-1};
     
 public:
+    
+    /*Creates Priority Queue of Specified Size */
+    PriorityQueue(size_t size);
+    
+    
+    
     /*Returns True if the PriorityQueue is Empty or else returns False .*/
     bool isEmpty() const;
     
@@ -30,10 +37,12 @@ public:
     bool isFull() const;
     
     
-    /*Enqueue's the Data according to its priority in the Queue.Returns True if the data is inserted Successfully in the Queue or else returns false .*/
+    /*Enqueue's the Data according to its priority in the Queue.Returns True if the data is inserted Successfully in the Queue or else returns false .
+        Throws Overflow Exception if the Queue is full and we try to insert data*/
     bool enQueue(T data);
     
-    /*Removes the Element with highest Priority from the Queue.*/
+    /*Removes the Element with highest Priority from the Queue.
+     Throws Underflow Exception if the Queue is Empty.*/
     bool deQueue();
     
     /* Returns Copy of Element with highest Priority .*/
@@ -43,6 +52,91 @@ public:
     /* Prints the Queue with elements in higher priority first to low priority  */
     void print() const;
 };
+
+
+template <typename T>
+PriorityQueue<T>::PriorityQueue(size_t size){
+    queueSize=size;
+}
+
+template <typename T>
+bool PriorityQueue<T>::isEmpty() const {
+    if(dataCount==-1){
+        return true;
+    }
+    return false;
+}
+
+
+template <typename T>
+bool PriorityQueue<T>::isFull() const{
+    if(dataCount==queueSize-1){
+        return true;
+    }
+    return false;
+}
+
+
+template <typename T>
+bool PriorityQueue<T>::enQueue(T data){
+    if(isFull()){
+        throw std::overflow_error("Queue is Full we can't insert more elements to the Queue ");
+    }
+    if(isEmpty()){
+        listOfData.push_back(data);
+        dataCount++;
+        return true;
+    }
+    auto queueIterator=listOfData.begin();
+    while (queueIterator!=listOfData.end()) {
+        
+        if(*queueIterator<data){
+            listOfData.insert(queueIterator, data);
+            dataCount++;
+            return true;
+        }
+        queueIterator++;
+    }
+    
+    listOfData.push_back(data);
+    dataCount++;
+    return true;
+    
+}
+
+
+template <typename T>
+void PriorityQueue<T>::print() const{
+    std::cout<<"Elements in the Priority Queue are as Follows : \n";
+    auto queueIterator=listOfData.cbegin();
+    while(queueIterator!=listOfData.cend()){
+        std::cout<<*queueIterator<<std::endl;
+        queueIterator++;
+    }
+}
+
+
+
+template <typename T>
+bool PriorityQueue<T>::deQueue(){
+    if(isEmpty()){
+        throw std::underflow_error("Queue is Empty Cannot DeQueue Elements ");
+    }
+
+    listOfData.pop_front();
+    dataCount--;
+    return true;
+}
+
+template <typename T >
+T PriorityQueue<T>::peek() const {
+    if(isEmpty()){
+        throw std::underflow_error("Queue is Empty ! Nothing to Peek In ");
+    }
+    T data=listOfData.front();
+    return data;
+}
+
 
 }
 
--- PriorityQueue/PriorityQueue/main.cpp
+++ PriorityQueue/PriorityQueue/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
-
+#include "PriorityQueue.hpp"
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    mylib::PriorityQueue<int> intQueue{3};
+    intQueue.enQueue(9);
+    intQueue.enQueue(1);
+    intQueue.enQueue(174);
+//    intQueue.enQueue(4);
+    intQueue.print();
+    std::cout<<"Is Queue Empty "<<std::boolalpha<<intQueue.isEmpty()<<std::endl;
+    std::cout<<"Is Queue Full "<<intQueue.isFull()<<std::endl;
+    std::cout<<"Peeking Element with Highest Priority in the Queue "<<intQueue.peek()<<std::endl;
+    intQueue.deQueue();
+    intQueue.print();
     return 0;
 }
